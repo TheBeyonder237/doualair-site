6114ee19a0e95bc29ecd50b82a134787c49c8108
Ngoue David Roger Yannick <david.ngoue@outlook.fr>
2024-12-12 18:47:15 +0100

    modification

--- D_News/settings.py
+++ D_News/settings.py
@@ -95,12 +95,8 @@ WSGI_APPLICATION = "D_News.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "doualair",
-        "USER": "postgres",
-        'PASSWORD': "Angeline2007",
-        "HOST": "localhost",
-        "PORT": "5432"
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
